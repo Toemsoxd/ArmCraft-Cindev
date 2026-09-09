bf0a9912a364669020a12933818babeefd07dd56
Toemsoxd <167713165+Toemsoxd@users.noreply.github.com>
2026-09-09 14:50:05 -0400

    Render engine framebuffer through CE GDI

---
+++ src/platform/wince_main.cpp
@@ -0,0 +1,118 @@
+#include <windows.h>
+#include "../engine.h"
+
+static ACEngine g_engine;
+static HWND g_window = NULL;
+
+static void AC_DrawFrame(HDC dc)
+{
+    BITMAPINFO bmi;
+    ZeroMemory(&bmi, sizeof(bmi));
+    bmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    bmi.bmiHeader.biWidth = g_engine.renderer.width;
+    bmi.bmiHeader.biHeight = -(LONG)g_engine.renderer.height;
+    bmi.bmiHeader.biPlanes = 1;
+    bmi.bmiHeader.biBitCount = 16;
+    bmi.bmiHeader.biCompression = BI_RGB;
+
+    SetStretchBltMode(dc, COLORONCOLOR);
+    StretchDIBits(
+        dc,
+        0, 0,
+        g_engine.renderer.width,
+        g_engine.renderer.height,
+        0, 0,
+        g_engine.renderer.width,
+        g_engine.renderer.height,
+        g_engine.renderer.pixels,
+        &bmi,
+        DIB_RGB_COLORS,
+        SRCCOPY);
+}
+
+static LRESULT CALLBACK AC_WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg) {
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC dc = BeginPaint(hwnd, &ps);
+            AC_DrawFrame(dc);
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+static ac_bool AC_PlatformCreateWindow(HINSTANCE instance)
+{
+    WNDCLASS wc;
+    ZeroMemory(&wc, sizeof(wc));
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = AC_WndProc;
+    wc.hInstance = instance;
+    wc.hbrBackground = (HBRUSH)(COLOR_BLACK + 1);
+    wc.lpszClassName = TEXT("ArmCraftCE");
+
+    if (!RegisterClass(&wc) && GetLastError() != ERROR_CLASS_ALREADY_EXISTS)
+        return AC_FALSE;
+
+    g_window = CreateWindow(
+        TEXT("ArmCraftCE"),
+        TEXT("ArmCraft CIndev"),
+        WS_VISIBLE,
+        0, 0, 320, 240,
+        NULL, NULL, instance, NULL);
+
+    return g_window != NULL ? AC_TRUE : AC_FALSE;
+}
+
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE previous, LPTSTR commandLine, int show)
+{
+    MSG msg;
+    DWORD lastTick;
+
+    (void)previous;
+    (void)commandLine;
+    (void)show;
+
+    if (!AC_EngineInit(&g_engine, 320, 240))
+        return 1;
+
+    if (!AC_PlatformCreateWindow(instance)) {
+        AC_EngineShutdown(&g_engine);
+        return 1;
+    }
+
+    lastTick = GetTickCount();
+
+    while (g_engine.running) {
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) {
+                g_engine.running = AC_FALSE;
+                break;
+            }
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        {
+            DWORD now = GetTickCount();
+            DWORD elapsed = now - lastTick;
+            lastTick = now;
+            AC_EngineTick(&g_engine, (ac_u32)elapsed);
+            InvalidateRect(g_window, NULL, FALSE);
+        }
+
+        Sleep(1);
+    }
+
+    AC_EngineShutdown(&g_engine);
+    return 0;
+}
